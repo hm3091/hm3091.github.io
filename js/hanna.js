f34c76273c1e0732835a1df08ac440219a96ee78
hm3091 <Hananeh Misaghi>
2019-04-06 00:17:40 +0430

    edit on images

--- js/hanna.js
+++ js/hanna.js
@@ -1,2 +1,2 @@
-var imageNumber=Math.floor(Math.random() *11) + 1;
+var imageNumber=Math.floor(Math.random() * 10) + 1;
 $('#sectionBackgroundImage').css("background-image", "url(img/hanna/bg" + imageNumber + ".jpg)");
