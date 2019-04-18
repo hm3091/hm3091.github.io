4b94bbc63acda74884da865579ddef902c1525d3
Misagh Moayyed <mmoayyed@unicon.net>
2019-04-18 05:58:33 -0700

    site updates

--- js/hanna.js
+++ js/hanna.js
@@ -1,18 +1,8 @@
 var imageNumber=Math.floor(Math.random() * 10) + 1;
 $('#sectionBackgroundImage').css("background-image", "url(img/hanna/bg" + imageNumber + ".jpg)");
 
-// fadeFunction = setTimeout(function()
-// {
-//     var brightness = $('#sectionBackgroundImage').css("filter");
-//     console.log(brightness);
 
-//     //$('#sectionBackgroundImage').css("filter", "brightness(45%)");
-//     //clearInterval(fadeFunction);
-// },
-// 1000);
-
-
-var changeBgFunc = setInterval(changeBackgroundFilter, 50);
+var changeBgFunc = setInterval(changeBackgroundFilter, 80);
 function changeBackgroundFilter() {
     var filter = $('#sectionBackgroundImage').css("filter");
     var first = filter.indexOf('(');
@@ -21,7 +11,7 @@ function changeBackgroundFilter() {
     value -= .1
     var newFilter = filter.substring(0, first + 1) + value + filter.substring(second);
     $('#sectionBackgroundImage').css("filter", newFilter);
-    if (value <= 0.45) {
+    if (value <= 0.40) {
         clearInterval(changeBgFunc)
     }
 }
