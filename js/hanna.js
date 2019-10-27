6ef6c90710c77a96adc34ccb73e7fa74e83510b6
Misagh Moayyed <mm1844@gmail.com>
2019-10-27 19:08:24 +0400

    remaining changes

--- js/hanna.js
+++ js/hanna.js
@@ -1,5 +1,30 @@
-var imageNumber=Math.floor(Math.random() * 10) + 1;
+/*********************************************************
+Change the total number of images to load images
+for the website background. Image are expected to be found
+inside the img/hanna directory using the filename format bg-{number}.jpg
+/**********************************************************/
+
+var totalNumberOfImages = 10
+var imageNumber=Math.floor(Math.random() * totalNumberOfImages) + 1;
 $('#sectionBackgroundImage').css("background-image", "url(img/hanna/bg" + imageNumber + ".jpg)");
+
+
+/*********************************************************
+Change slogans by adding them below, separate them by a comma
+/**********************************************************/
+
+const slogans = [
+    "Stylish Architectural Designs and Professional Graphics",
+    "Professional Architectural Designs with Stylish Graphics"
+];
+
+var sloganNumber=Math.floor(Math.random() * slogans.length);
+$('#sloganText').text(slogans[sloganNumber]);
+
+/*********************************************************
+The logic below is used to apply shade and filters animations
+as the background picture loads. Do not modify!
+/**********************************************************/
 
 var changeBgFunc = setInterval(changeBackgroundFilter, 80);
 function changeBackgroundFilter() {
@@ -10,7 +35,7 @@ function changeBackgroundFilter() {
     value -= .1
     var newFilter = filter.substring(0, first + 1) + value + filter.substring(second);
     $('#sectionBackgroundImage').css("filter", newFilter);
-    if (value <= 0.40) {
+    if (value <= 0.38) {
         clearInterval(changeBgFunc)
     }
 }
