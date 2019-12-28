44f7e14c1ed2aa8005694f71875f57f94d364568
Misagh Moayyed <mm1844@gmail.com>
2019-12-28 23:05:34 +0400

    updates

---
+++ js/portfolio.js
@@ -0,0 +1,44 @@
+var portfolioItemsForHome = [
+    {
+        link: 'img/portfolio-home/1.jpg',
+        images: [],
+        thumbnail: 'img/portfolio-home/1.jpg',
+        title: 'Chitgar Connection Bridge',
+        description: 'As the largest metropolitan area in Tehran, this project is about connecting the park and the lake in the Chitgar Recreational Zone.'
+    },
+    {
+        link: 'img/portfolio-home/2.jpg',
+        thumbnail: 'img/portfolio-home/4.jpg',
+        images: ['img/portfolio-home/2.jpg', 'img/portfolio-home/4.jpg'],
+        title: 'Salian Office Building',
+        description: 'Salian is an Iranian clothing brand. This design aims to transform the building from a private isolated mass into a multipurpose structure.'
+    },
+    {
+        link: 'img/portfolio-home/5.jpg',
+        thumbnail: 'img/portfolio-home/5.jpg',
+        images: ['img/portfolio-home/6.jpg', 'img/portfolio-home/7.jpg'],
+        title: 'Majidieh House',
+        description: 'Renovations for a one-floor modest villa in north of Tehran.'
+    },
+    {
+        link: 'img/portfolio-home/8.jpg',
+        thumbnail: 'img/portfolio-home/8.jpg',
+        images: ['img/portfolio-home/9.jpg'],
+        title: 'Eghbaliye Garden City',
+        description: 'The main purpose of this project is to organize the city of Eghbaliye, to review the city’s livelihood, economic, social and demographic conditions.'
+    },
+    {
+        link: 'img/portfolio-home/10.jpg',
+        thumbnail: 'img/portfolio-home/10.jpg',
+        images: [],
+        title: 'Mediatheque',
+        description: 'A Mediatheque complex, art gallery, cinema and museum, functioning as a media library, exhibition space and recreational area as proxies for social congregations.'
+    },
+    {
+        link: 'img/portfolio-home/11.jpg',
+        thumbnail: 'img/portfolio-home/11.jpg',
+        images: [],
+        title: 'Qaban Museum',
+        description: 'The museum is inspired by the Qaban castle in Maku, in the West Azerbaijan Province, and is tasked to protect and preserve the remains of the castle relics.'
+    }
+]
