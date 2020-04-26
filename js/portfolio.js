15ccadea618ad4c35f9954ee15384320317ec87d
Misagh Moayyed <mm1844@gmail.com>
2020-04-26 21:44:20 +0430

    fixes

--- js/portfolio.js
+++ js/portfolio.js
@@ -9,28 +9,28 @@ var portfolioItemsForHome = [
     {
         link: 'portfolio-salian-offices.html',
         thumbnail: 'img/portfolio-home/2.jpg',
-        images: ['img/portfolio-home/3.jpg', 'img/portfolio-home/4.jpg', 'img/portfolio-home/5.jpg', 'img/portfolio-home/6.jpg', 'img/portfolio-home/7.jpg', 'img/portfolio-home/8.jpg', 'img/portfolio-home/9.jpg', 'img/portfolio-home/10.jpg'],
+        images: [],
         title: 'Salian Office Building',
         description: 'Salian is an Iranian clothing brand. This design aims to transform the building from a private isolated mass into a multipurpose structure.'
     },
     {
         link: 'portfolio-majidieh-house.html',
         thumbnail: 'img/portfolio-home/11.jpg',
-        images: ['img/portfolio-home/12.jpg', 'img/portfolio-home/13.jpg'],
+        images: [],
         title: 'Majidieh House',
         description: 'Renovations for a one-floor modest villa in north of Tehran.'
     },
     {
         link: 'portfolio-eghbaliyeh-gardencity.html',
         thumbnail: 'img/portfolio-home/14.jpg',
-        images: ['img/portfolio-home/15.jpg'],
+        images: [],
         title: 'Eghbaliye Garden City',
         description: 'The main purpose of this project is to organize the city of Eghbaliye, to review the city’s livelihood, economic, social and demographic conditions.'
     },
     {
         link: 'portfolio-qaban-museum.html',
         thumbnail: 'img/portfolio-home/16.jpg',
-        images: ['img/portfolio-home/17.jpg', 'img/portfolio-home/18.jpg'],
+        images: [],
         title: 'Qaban Museum',
         description: 'The museum is inspired by the Qaban castle in Maku, in the West Azerbaijan Province, and is tasked to protect and preserve the remains of the castle relics.'
     },
@@ -51,28 +51,28 @@ var portfolioItemsForHome = [
     {
         link: 'portfolio-chitgar-connection-bridge.html',
         thumbnail: 'img/portfolio-home/21.jpg',
-        images: ['img/portfolio-home/22.jpg', 'img/portfolio-home/23.jpg'],
+        images: [],
         title: 'Chitgar Connection Bridge',
         description: 'As the largest metropolitan area in Tehran, this project is about connecting the park and the lake in the Chitgar Recreational Zone.'
     },
     {
-        link: 'img/portfolio-home/24.jpg',
+        link: 'portfolio-new-plasco.html',
         thumbnail: 'img/portfolio-home/24.jpg',
-        images: ['img/portfolio-home/25.jpg', 'img/portfolio-home/35.jpg'],
+        images: [],
         title: 'New Plasco',
         description: 'The Plasco building’s exterior design is inspired by a new concept as well as the tragic fire accident. We try to connect the pathways and use the ceiling of the 5-story building to house more people into the building and use the spaces as the museum to pay tribute to fire-fighters who died in the fire explosion.'
     },
     {
         link: 'portfolio-dezashib-offices.html',
         thumbnail: 'img/portfolio-home/26.jpg',
-        images: ['img/portfolio-home/27.jpg'],
+        images: [],
         title: 'Dezashib Offices',
         description: 'A renovation project where residential spaces are transformed into office buildings. The main design goal is to present a new concept to allow for introduction of green spaces while also exposing the corridors and staircases to the public view.'
     },
     {
         link: 'portfolio-mehrshahr-villa.html',
         thumbnail: 'img/portfolio-home/28.jpg',
-        images: ['img/portfolio-home/29.jpg', 'img/portfolio-home/30.jpg', 'img/portfolio-home/31.jpg', 'img/portfolio-home/32.jpg', 'img/portfolio-home/33.jpg', 'img/portfolio-home/34.jpg'],
+        images: [],
         title: 'Mehrshahr Villa',
         description: 'A family villa, from a collection of 4 villas where the main design idea is to introduce new open spaces for the family to include both private and public spaces, separately.'
     },
@@ -98,7 +98,6 @@ function writePortfolioWorkItemsForHome() {
                 <a href='$link'>
                     <img src='$image' title='pdescription' alt='pdescription' class='work-item__img'>
                 </a>
-                $images
                 </div>
                 <div class='card__body'>
                 <div class='work-item__description'>
@@ -111,16 +110,14 @@ function writePortfolioWorkItemsForHome() {
 
         var copiedItems = shuffle(portfolioItemsForHome.slice());
         copiedItems.forEach(element => {
-            var images = "<a href='" + element.thumbnail + "' class='lightbox-img'></a>";
+           
             var allThumbnails = [element.thumbnail];
             for (let idx = 0; idx < element.images.length; idx++) {
-                images += "<a href='" + element.images[idx] + "' class='lightbox-img'></a>";
                 allThumbnails.push(element.images[idx]);
             }
             var thumbnailIdx = Math.floor(Math.random() * allThumbnails.length);
             var selectedThumbnail = allThumbnails[thumbnailIdx];
             var html = template.replace("$link", element.link)
-                .replace("$images", images)
                 .replace("$image", selectedThumbnail)
                 .replace("$title", element.title)
                 .replace(/pdescription/g, element.description);
