887e802788d538b89ce7b08170066f1c75c52121
hm3091 <hm3091@gmail.com>
2022-03-13 23:55:11 +0400

    test

--- js/portfolio.js
+++ js/portfolio.js
@@ -65,7 +65,6 @@ function writePortfolioWorkItemsForHome() {
                 </div>
             </div>
         </div>`;
-
         var copiedItems = shuffle(portfolioItemsForHome.slice());
         copiedItems.forEach(element => {
            
